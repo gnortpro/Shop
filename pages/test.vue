7c264013a0cbe4518c969181dbf8511927e3055a
Trong <gnortpro4@gmail.com>
2019-06-27 23:51:36 +0700

    init

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="test()"></button>
+</template>
+
+<script>
+export default {
+  methods: {
+    test() {
+      this.$store.dispatch("getProducts");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
