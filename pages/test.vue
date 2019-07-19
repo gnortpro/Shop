96ae35cc4b5dd6bc5e5983d7744d136b8ff4fd78
Trong <trongnguyen@x3english.com>
2019-07-19 15:50:22 +0700

    tets

--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container" id="app">
+    <!-- <div class="container" id="app">
       <div class="form-group">
         <label>
           <button class="btn btn-primary btn-sm" @click.prevent="playSound(s)">
@@ -9,17 +9,24 @@
           Play Air Plane Ding
         </label>
       </div>
-    </div>
+    </div>-->
+    <audio controls>
+      <source
+        src="https://download1484.mediafire.com/6fphf70i816g/c28g9mkh07j2o9l/Dua-Nhau-Di-Tron-Chill-Version-Den-Linh-Cao.ogg"
+        type="audio/ogg"
+      />
+      <source
+        src="https://download1349.mediafire.com/7vknsbevs6hg/p26v2oamjq0vmiy/Dua-Nhau-Di-Tron-Chill-Version-Den-Linh-Cao.mp3"
+        type="audio/mpeg"
+      />Your browser does not support the audio element.
+    </audio>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      s:
-        'https://vnno-vn-6-tf-mp3-s1-zmp3.zadn.vn/8fd0d56eae2a47741e3b/5253865936530607617?authen=exp=1563605386~acl=/8fd0d56eae2a47741e3b/*~hmac=d4d2d44e02dba8edbcd25c5c664b7f43&filename=Dua-Nhau-Di-Tron-Chill-Version-Den-Linh-Cao.mp3'
-    }
+    return {}
   },
   methods: {
     playSound(sound) {
